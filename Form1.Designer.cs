eb7168b154389ed89cc7271672450d78ba7e45da
SAC-play <hyojin.jd.park@gmail.com>
2023-03-22 00:34:15 +0900

    add time unit select, input work name User Interface

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            textBox1 = new TextBox();
+            button1 = new Button();
+            TimeUnitUpDown = new DomainUpDown();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(133, 164);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(500, 23);
+            textBox1.TabIndex = 0;
+            textBox1.KeyDown += textBox_KeyDown;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(648, 164);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 23);
+            button1.TabIndex = 1;
+            button1.Text = "입  력";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += MouseClickOk;
+            // 
+            // TimeUnitUpDown
+            // 
+            TimeUnitUpDown.Location = new Point(133, 100);
+            TimeUnitUpDown.Name = "TimeUnitUpDown";
+            TimeUnitUpDown.ReadOnly = true;
+            TimeUnitUpDown.Size = new Size(141, 23);
+            TimeUnitUpDown.TabIndex = 2;
+            TimeUnitUpDown.SelectedItemChanged += TimeUnitItemChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(TimeUnitUpDown);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "LetsGoHaeNi";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private DomainUpDown TimeUnitUpDown;
+        public string[] m_str_time_unit = { "15 min", "30 min", "60 min" };
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace LTH
 {
     public partial class Form1 : Form
@@ -5,6 +7,35 @@ namespace LTH
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void MouseClickOk(object sender, EventArgs e)
+        {
+            //work name input
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //work name input
+            }
+        }
+
+        private void TimeUnitItemChanged(object sender, EventArgs e)
+        {
+            if (TimeUnitUpDown.SelectedItem.ToString() == m_str_time_unit[0])
+            {
+                //15 min
+            }
+            else if (TimeUnitUpDown.SelectedItem.ToString() == m_str_time_unit[1])
+            {
+                //30 min
+            }
+            else
+            {
+                //60 min
+            }
         }
     }
 }
